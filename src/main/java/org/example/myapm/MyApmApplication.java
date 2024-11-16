5c3337635bd190529daf6a207c65442af52a7629
huiseung <whtjddn75@gmail.com>
2024-11-16 23:24:13 +0900

    [config] java project init

---
+++ src/main/java/org/example/myapm/MyApmApplication.java
@@ -0,0 +1,13 @@
+package org.example.myapm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyApmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyApmApplication.class, args);
+	}
+
+}
