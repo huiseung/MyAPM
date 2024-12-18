4f57f681065d7eb7e7f73ed7a52f6b0c821dec21
huiseung <whtjddn75@gmail.com>
2024-12-18 22:53:33 +0900

    [style] toString 보기 편하게 개행 추가

--- common/src/main/java/org/example/common/context/Span.java
+++ common/src/main/java/org/example/common/context/Span.java
@@ -5,27 +5,54 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Span {
-    private final String methodName;
-    private final long startTime;
+    private String methodName;
+    private long startTime;
     private long endTime;
     private long duration;
     private final List<Span> childs = new ArrayList<Span>();
+
+    public Span() {
+    }
 
     public Span(String methodName, long startTime) {
         this.methodName = methodName;
         this.startTime = startTime;
     }
 
+    public String getMethodName() {
+        return methodName;
+    }
+
     public long getStartTime() {
         return startTime;
+    }
+
+    public long getEndTime() {
+        return endTime;
+    }
+
+    public long getDuration() {
+        return duration;
+    }
+
+    public List<Span> getChilds() {
+        return childs;
+    }
+
+    public void setMethodName(String methodName) {
+        this.methodName = methodName;
+    }
+
+    public void setStartTime(long startTime) {
+        this.startTime = startTime;
     }
 
     public void setEndTime(long endTime) {
         this.endTime = endTime;
     }
 
-    public void setDuration(long durationNano) {
-        this.duration = durationNano;
+    public void setDuration(long duration) {
+        this.duration = duration;
     }
 
     public void addChild(Span child) {
@@ -40,7 +67,7 @@ public class Span {
                 .append(", startTime=").append(startTime)
                 .append(", endTime=").append(endTime)
                 .append(", duration=").append(duration).append(" ms")
-                .append(", childs=").append(childs)
+                .append(", \nchilds=").append(childs)
                 .append('}');
         return sb.toString();
     }
